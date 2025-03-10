3d3a0113c2f56a6e5dfc835eef57540c8b36c9f6
Eleanore <eleanorez1922@gmail.com>
2025-03-10 02:06:53 -0700

    Add project files.

---
+++ project4/fm.h
@@ -0,0 +1,28 @@
+#ifndef FM_INCLUDED
+#define FM_INCLUDED
+
+#include "provided.h"
+
+class FlightManager
+{
+public:
+	FlightManager()
+	{
+
+	}
+	~FlightManager()
+	{
+
+	}
+	bool load_flight_data(std::string filename)
+	{
+
+	}
+	std::vector<FlightSegment> find_flights
+	(std::string source_airport, int start_time, int end_time) const
+	{
+
+	}
+};
+
+#endif
---
+++ project4/tp.h
@@ -0,0 +1,28 @@
+#ifndef TP_INCLUDED
+#define TP_INCLUDED
+
+class TravelPlanner
+{
+public:
+	TravelPlanner(const FlightManagerBase& flight_manager,
+		const AirportDB& airport_db)
+	{
+
+	}
+	~TravelPlanner()
+	{
+
+	}
+	void add_preferred_airline(std::string airline)
+	{
+
+	}
+	bool plan_travel(std::string source_airport, std::string
+		destination_airport, int start_time, Itinerary& itinerary)
+		const
+	{
+
+	}
+};
+
+#endif
